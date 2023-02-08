41728e1dddb33a9dcf9838333c403195ac46d7a0
Emmanuel <emmanueljoe777@gmail.com>
2023-02-08 22:53:31 +0100

    first commit

---
+++ todo-/app.js
@@ -0,0 +1,230 @@
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const check = document.querySelector(".check");
+const checkImg = document.querySelector(".check-img");
+const trackSpan = document.querySelector(".track span");
+const filterOptions = document.querySelector(".filter-todo");
+const clearCompleted = document.querySelector(".clear");
+const all = document.querySelector(".all");
+const active = document.querySelector(".active");
+const completed = document.querySelector(".completed");
+// const clearAll = document.querySelector(".clear-all");
+const toggleMode = document.querySelector(".toggle-mode");
+const moon = document.querySelector(".moon");
+const sun = document.querySelector(".sun");
+const heroImg = document.querySelector(".hero-img img");
+const darkImg = document.querySelector(".dark-img");
+const lightImg = document.querySelector(".light-img");
+const root = document.querySelector(":root");
+const html = document.querySelector("html");
+
+document.addEventListener("DOMContentLoaded", getTodos);
+
+todoButton.addEventListener("click", addTodo);
+
+todoList.addEventListener("click", deleteTodo);
+
+filterOptions.addEventListener("click", filterTodo);
+
+// clearAll.addEventListener("click", clearTodos);
+
+sun.addEventListener("click", lightMode);
+
+moon.addEventListener("click", darkMode);
+
+html.classList.add("chromium-dark");
+html.classList.add("firefox-dark");
+
+function lightMode() {
+  moon.classList.add("display-moon");
+  sun.classList.add("slide-sun");
+  darkImg.classList.add("hide-img");
+  lightImg.classList.add("show-img");
+  root.style.setProperty("--VeryDarkBlue", "hsl(0, 0%, 98%)");
+  root.style.setProperty("--VeryDarkDesaturatedBlue", "hsl(0, 0%, 98%)");
+  root.style.setProperty("--LightGrayishBlue", "hsl(235, 24%, 19%)");
+  root.style.setProperty("--VeryLightGrayishBlue", "hsl(235, 19%, 35%)");
+  root.style.setProperty("--VeryDarkGrayishBlue", "hsl(236, 9%, 61%)");
+  root.style.setProperty("--darkshadow", "#585656bd");
+  html.classList.remove("firefox-light");
+  html.classList.remove("chromium-dark");
+}
+
+function darkMode() {
+  moon.classList.remove("display-moon");
+  sun.classList.remove("slide-sun");
+  darkImg.classList.remove("hide-img");
+  lightImg.classList.remove("show-img");
+  root.style.removeProperty("--VeryDarkBlue", "hsl(0, 0%, 98%)");
+  root.style.removeProperty("--VeryDarkDesaturatedBlue", "hsl(0, 0%, 98%)");
+  root.style.removeProperty("--LightGrayishBlue", "hsl(235, 24%, 19%)");
+  root.style.removeProperty("--VeryLightGrayishBlue", "hsl(235, 19%, 35%)");
+  root.style.removeProperty("--VeryDarkGrayishBlue", "hsl(236, 9%, 61%)");
+  root.style.removeProperty("--darkshadow", "#585656bd");
+  html.classList.add("firefox-dark");
+  html.classList.add("chromium-dark");
+}
+
+function addTodo(event) {
+  event.preventDefault();
+
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  const completedBtn = document.createElement("button");
+  completedBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+  completedBtn.classList.add("complete-btn");
+  todoDiv.appendChild(completedBtn);
+
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  saveLocalTodos(todoInput.value);
+
+  const trashBtn = document.createElement("div");
+  trashBtn.innerHTML = '<img src="./images/icon-cross.svg" alt="icon-cross">';
+  trashBtn.classList.add("trash");
+  todoDiv.appendChild(trashBtn);
+
+  todoList.appendChild(todoDiv);
+
+  todoInput.value = "";
+}
+
+function deleteTodo(e) {
+  const item = e.target;
+  //DELETE TODO
+  if (item.classList[0] === "trash") {
+    const todo = item.parentElement;
+    todo.classList.add("fall");
+    //ANIMATION
+    removeLocalTodos(todo);
+
+    todo.addEventListener("transitionend", (e) => {
+      todo.remove();
+    });
+  }
+
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed-todo");
+    item.classList.toggle("checked");
+  }
+}
+
+// function clearTodos(e) {
+//   const clearBtn = e.target;
+//   const todos = clearBtn.parentElement.previousElementSibling.children;
+//   for (i = 0; i < todos.length; i++) {
+//     todos[i].style.display = "none";
+//     todos[i].remove();
+//   }
+// }
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (e.target.classList.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "active":
+        if (!todo.classList.contains("completed-todo")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "completed":
+        if (todo.classList.contains("completed-todo")) {
+          todo.style.display = "flex";
+          clearCompleted.addEventListener("click", function () {
+            todo.classList.add("fall");
+            todo.addEventListener("transitionend", (e) => {
+              todo.remove();
+            });
+          });
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
+}
+
+filterOptions.addEventListener("click", function (e) {
+  if (e.target === all) {
+    all.style.color = "#2de674";
+    active.style.color = "#777a92";
+    completed.style.color = "#777a92";
+  } else if (e.target === active) {
+    active.style.color = "#2de674";
+    all.style.color = "#777a92";
+    completed.style.color = "#777a92";
+  } else if (e.target === completed) {
+    completed.style.color = "#2de674";
+    active.style.color = "#777a92";
+    all.style.color = "#777a92";
+  }
+});
+
+function saveLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function saveDarkMode(mode) {
+  mode = JSON.parse(localStorage.getItem("mode"))
+  localStorage.setItem("mode", JSON.stringify(mode))
+}
+
+function getTodos() {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.forEach(function (todo) {
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    const completedBtn = document.createElement("button");
+    completedBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+    completedBtn.classList.add("complete-btn");
+    todoDiv.appendChild(completedBtn);
+
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todo;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+
+    const trashBtn = document.createElement("div");
+    trashBtn.innerHTML = '<img src="./images/icon-cross.svg" alt="icon-cross">';
+    trashBtn.classList.add("trash");
+    todoDiv.appendChild(trashBtn);
+
+    todoList.appendChild(todoDiv);
+  });
+}
+
+function removeLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  const todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex), 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
